1fdc89851f5eef7e47faba4bb78efeadec529191
ahazain <ahmadfariddd5@gmail.com>
2025-01-08 23:05:10 +0700

    dokumentasi-register

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,9 +2,7 @@ const express = require("express");
 const router = express.Router();
 authControllers = require("../controllers/authControllers");
 
-
 router.post("/register", authControllers.register);
 router.post("/login", authControllers.login);
-
 
 module.exports = router;
